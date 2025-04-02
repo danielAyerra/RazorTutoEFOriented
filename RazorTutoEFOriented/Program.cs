abef230db98d25043bf6be89429f7119ab16e4f9
Daniel Ayerra García <danyerra@gmail.com>
2025-04-02 14:57:20 +0200

    Prepare Dev env for better debugging, created a db Initializer

--- RazorTutoEFOriented/Program.cs
+++ RazorTutoEFOriented/Program.cs
@@ -10,6 +10,7 @@ if (builder.Environment.IsDevelopment())
 {
     builder.Services.AddDbContext<SchoolContext>(options =>
         options.UseSqlite(builder.Configuration.GetConnectionString("SchoolContext") ?? throw new InvalidOperationException("Connection string 'SchoolContext' not found.")));
+    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 }
 else
 {
@@ -28,6 +29,21 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
+}
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<SchoolContext>();
+    context.Database.EnsureCreated();
+    DbInitializer.Initialize(context);
 }
 
 app.UseHttpsRedirection();
