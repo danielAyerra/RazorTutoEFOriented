8d797c332a9b733131fdc564b341a2d55a90a46f
Daniel Ayerra García <danyerra@gmail.com>
2025-04-03 16:29:31 +0200

    Added Pagination.

--- RazorTutoEFOriented/Pages/Students/Index.cshtml.cs
+++ RazorTutoEFOriented/Pages/Students/Index.cshtml.cs
@@ -1,22 +1,26 @@
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using RazorTutoEFOriented.Data;
 using RazorTutoEFOriented.Models;
+using RazorTutoEFOriented.Tools;
 
 namespace RazorTutoEFOriented.Pages.Students
 {
     public class IndexModel : PageModel
     {
         private readonly SchoolContext _context;
-
-        public IndexModel(SchoolContext context)
+        private readonly IConfiguration _configuration;
+        public IndexModel(SchoolContext context,  IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
 
         public string NameSort { get; set; }
@@ -24,13 +28,23 @@ namespace RazorTutoEFOriented.Pages.Students
         public string CurrentFilter { get; set; }
         public string CurrentSort { get; set; }
 
-        public IList<Student> Students { get; set; }
+        public PaginatedList<Student> Students { get; set; }
 
-        public async Task OnGetAsync(string sortOrder, string searchString)
+        public async Task OnGetAsync(string sortOrder, string searchString, string currentFilter, int? pageIndex)
         {
+            CurrentSort = sortOrder;
             NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : String.Empty;
             DateSort = sortOrder == "Date" ? "date_desc" : "Date";
 
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            
             CurrentFilter = searchString;
             
             IQueryable<Student> studentsIQ = from s in _context.Students select s;
@@ -55,8 +69,10 @@ namespace RazorTutoEFOriented.Pages.Students
                     studentsIQ = studentsIQ.OrderBy(s => s.LastName);
                     break;
             }
+            
+            var pageSize = _configuration.GetValue<int>("PageSize", 4);
 
-            Students = await studentsIQ.AsNoTracking().ToListAsync();
+            Students = await PaginatedList<Student>.CreateAsync(studentsIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
         }
     }
 }
